bd06ac6807d82ffcf5d70f705fc1fcca9abeb1ac
Ronald Tapia <ronald.trd@gmail.com>
2022-04-04 22:57:00 -0500

    proyecto_inicial

---
+++ app/src/main/java/com/usat/android_gas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.usat.android_gas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
